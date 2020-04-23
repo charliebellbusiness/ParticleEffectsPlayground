1e780d8c5bddfba32089e1050d26b3c6c7501069
Charles Bell <charlesbell@MacBook-Pro-23.local>
2020-04-22 23:24:38 -0500

    Added centered text to home page, additional comments for clarity

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const particles = []
+const particles = [];
+const vW = window.innerWidth;
+const vH = window.innerHeight;
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
 
-    const particlesLength = Math.floor(window.innerWidth / 10);
+    createCanvas(vW, vH);
+
+    const particlesLength = Math.floor(vW / 10);
 
     for(let i = 0; i < particlesLength; i++) {
         particles.push(new Particle());
@@ -11,13 +14,36 @@ function setup() {
 }
 
 function draw() {
-    background(55, 100, 144)
     
+    // Draw the background over and over to prevent streaking of particle path. 
+    
+    //There has to be a more optimized way to do this because I am SO tired of my MacBook running like a jet engine when the particle count gets higher. Optimize the background function AND LET THE PARTICLE CHAOS BEGIN.
+
+    background(55, 100, 144)
+
+    // Setting constants for referencing element context and element more easily
+    var ctx = document.getElementById('defaultCanvas0').getContext('2d'); //Reference 2d context
+    var canvas =  document.getElementById('defaultCanvas0') //Reference canvas
+
+    // Canvas font styling
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'bottom';
+    ctx.font = '5vw helvetica';
+    
+    // Get the Font Height of the text for future use
+    var approxFontHeight = parseInt(ctx.font);
+
+    // Use the visual width to center horizontally, then use visual height and font height to center text vertically. Font height is to negate text font offset.
+    ctx.fillText('particle effects playground', vW / 2, (vH / 2) + approxFontHeight / 4)
+
+    // For each particle, reference Particle class functions listed below
     particles.forEach((p, index) => {
         p.update();
         p.draw();
         p.checkParticles(particles.slice(index));
     })
+
+
 
 }
 
@@ -43,7 +69,7 @@ class Particle {
 
     //Draw single particle
     draw() { 
-        noStroke()
+        noStroke();
         fill(
             // 'rgba(' + Math.floor(random(0, 255)) + ',' + Math.floor(random(0, 255)) + ',' + Math.floor(random(0,255)) + ', 0.5)' 
             // SEIZURE WARNING
@@ -67,7 +93,7 @@ class Particle {
         }
     }
 
-    //Connect lines between particles
+    //Connect lines between particles by checking distance
     checkParticles() {
         particles.forEach(particle => {
             const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
