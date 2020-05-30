74e680ff786c732f48e7c7351e5428d247c97c84
Charles Bell <charlesbell@MacBook-Pro-23.local>
2020-05-30 14:08:33 -0500

    Added a changing message based on particle amount on screen. Preparing for moving useful functions to global access file for import/export. Plans to add more games and create global functions

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function draw() {
     background(55, 100, 144)
 
     // Setting constants for referencing element context and element more easily
-    var ctx = document.getElementById('defaultCanvas0').getContext('2d'); //Reference 2d context
+    var ctx = document.getElementById('defaultCanvas0').getContext('2d'); //Reference 2d context of canvas
     var canvas =  document.getElementById('defaultCanvas0') //Reference canvas
 
     // Canvas font styling
@@ -33,14 +33,27 @@ function draw() {
     // Get the Font Height of the text for future use
     var approxFontHeight = parseInt(ctx.font);
 
-    // Use the visual width to center horizontally, then use visual height and font height to center text vertically. Font height is to negate text font offset.
-    ctx.fillText('particle effects playground', vW / 2, (vH / 2) + approxFontHeight / 4)
+    // Use the visual width to center horizontally, then use visual height and font height to center text vertically. Font height is to negate text font offset. Message changes based on amount of particles not undefined
+
+    function checkParticlesUndefined(particle) {
+       return particle != undefined;
+    }
+
+    // Get length of particles that are not undefined.
+    if (particles.filter(checkParticlesUndefined).length > particles.length / 2) {
+
+    ctx.fillText('welcome to particle effects playground', vW / 2, (vH / 2) + approxFontHeight / 4)
+
+    } else { ctx.fillText('choose more physics games above', vW / 2, (vH / 2) + approxFontHeight / 4) }
+
+    console.log(particles.filter(checkParticlesUndefined).length)
+    console.log(particles.length)
 
     // For each particle, reference Particle class functions listed below
-    particles.forEach((p, index) => {
+    particles.forEach((p) => {
         p.update();
         p.draw();
-        p.checkParticles(particles.slice(index));
+        p.checkParticles();
     })
 
 
@@ -55,7 +68,7 @@ class Particle {
         this.pos = createVector(random(width), random(height));
 
         //Velocity
-        this.vel = createVector(random(-2, 2), random(-2, 2));
+        this.vel = createVector(random(-1, 1), random(-1, 1));
 
         // Size
         this.size = random(1, 10);
@@ -78,22 +91,28 @@ class Particle {
         circle(this.pos.x, this.pos.y, this.size)
     }
 
+    // Detect if particle touches edge of canvas
     edges() {
         
         if(this.pos.x < 0 || this.pos.x > width) {
             this.vel.x *= -1;
 
-            delete particles[particles.indexOf(this)]; //Get index of this particle and delete it
-        }
+            var particleIndex = particles.indexOf(this); //Get index of this particle
 
-        if(this.pos.y < 0 || this.pos.y > height) {
+            delete particles[particleIndex];
+
+        } else if(this.pos.y < 0 || this.pos.y > height) {
             this.vel.y *= -1;
             
-            delete particles[particles.indexOf(this)]; //Get index of this particle and delete it
+            var particleIndex = particles.indexOf(this);
+
+            delete particles[particleIndex]; //Get index of this particle and delete it
+
         }
+
     }
 
-    //Connect lines between particles by checking distance
+    // Get statuses of particles such as position, amount, velocity.
     checkParticles() {
         particles.forEach(particle => {
             const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
@@ -102,6 +121,8 @@ class Particle {
                 stroke('rgba(255,255,50, 0.1)')
                 line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
             }
+
         })
+        
     }
 }
